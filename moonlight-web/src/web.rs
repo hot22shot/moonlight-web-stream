c2e1be9c0f88fd4249bc7683b320563c35bd87ec
MrCreativ3001 <92984089+MrCreativ3001@users.noreply.github.com>
2025-08-01 14:58:18 +0200

    simple auth

--- moonlight-web/src/web.rs
+++ moonlight-web/src/web.rs
@@ -1,11 +1,12 @@
-use actix_web::{HttpResponse, Responder, dev::HttpServiceFactory, get, services};
+use actix_files::Files;
+use actix_web::dev::HttpServiceFactory;
 
 pub fn web_service() -> impl HttpServiceFactory {
-    services![index]
-}
+    #[cfg(debug_assertions)]
+    let files = Files::new("/", "dist").index_file("index.html");
 
-#[get("/")]
-async fn index() -> impl Responder {
-    // TODO: host using dist or static
-    HttpResponse::Ok().body(include_str!("../dist/index.html"))
+    #[cfg(not(debug_assertions))]
+    let files = Files::new("/", "static").index_file("index.html");
+
+    files
 }
--- moonlight-web/web/index.ts
+++ moonlight-web/web/index.ts
@@ -1,2 +1,39 @@
+import { Api, authenticate } from "./api.js"
 
-const credentials = window.prompt("credentials")
+// TODO: error handler with popup
+
+async function getApi(host_url?: string): Promise<Api> {
+    if (!host_url) {
+        host_url = `${window.location.origin}/api`;
+    }
+
+    let credentials = window.sessionStorage.getItem("credentials");
+
+    if (credentials != null) {
+        return { host_url, credentials };
+    }
+
+    credentials = window.prompt("credentials")
+    if (credentials == null) {
+        const error = "please reload and enter valid credentials";
+        alert(error);
+
+        throw error
+    }
+
+    const api = { host_url, credentials };
+
+    authenticate(api)
+
+    window.sessionStorage.setItem("credentials", credentials);
+
+    return api;
+}
+
+async function startApp() {
+    const api: Api = await getApi();
+
+}
+
+console.log("starting app")
+startApp()
